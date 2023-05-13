63727acdb27a5ef6766996e7b533ba559145fd8c
cy-bug <shashwat5590@gmail.com>
2023-05-14 04:15:27 +0530

    app completed.

---
+++ pages/api/generate.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { text } = req.body;
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `explain the given prompt like i'm a toddler (max: 300 words, min: 100 words), give simple analogy for the same if applicable (important: wrap headings in <h3> tag and text in <p> tag). Next I want you to give me a json code for a 5 question quiz (with 4 options and correct answer, do not generate any titles for json) wrapped inside like this "••• {json code} ••• "
+
+      Topic —> ${text}
+
+      `,
+      temperature: 0.7,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      max_tokens: 1500,
+    });
+
+    const generatedText = completion.data.choices[0].text;
+    res.status(200).json({ result: generatedText });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+}
--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,11 +1,15 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/no-unescaped-entities */
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./App.module.css";
 import { RemoveCurtain } from "@/components/curtain/Curtain";
 import Head from "next/head";
+import { gsap } from "gsap";
 
 const App = () => {
 	const inputRef = React.useRef<HTMLInputElement>(null);
+	const loadingRef = React.useRef<HTMLDivElement>(null);
+	const [valid, setValid] = React.useState(true);
 
 	React.useEffect(() => {
 		const handleLoad = () => RemoveCurtain();
@@ -51,6 +55,111 @@ const App = () => {
 		return () => clearInterval(interval);
 	}, []);
 
+	const btnRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const btn = btnRef.current;
+		if (btn) {
+			btn.addEventListener("click", handleProceed);
+		}
+		return () => {
+			if (btn) {
+				btn.removeEventListener("click", handleProceed);
+			}
+		};
+	}, []);
+
+	const handleProceed = async () => {
+		if (!valid) return;
+		inputRef.current?.setAttribute("disabled", "true");
+		loadingRef.current?.style.setProperty("visibility", "visible");
+
+		const inputValue = inputRef.current?.value;
+		if (inputValue) {
+			try {
+				console.log("sending request!");
+				setValid(false);
+				const response = await fetch("/api/generate", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ text: inputValue }),
+				});
+				const data = await response.json();
+				inputRef.current?.removeAttribute("disabled");
+				loadingRef.current?.style.setProperty("visibility", "hidden");
+				var pogg = handleData(data);
+				if (pogg) {
+					updateHTML(pogg[0]);
+					console.log(pogg[1]);
+					setValid(true);
+				}
+
+			} catch (error) {
+				console.error("Error:", error);
+			}
+		}
+	};
+
+	function handleData(data: any) {
+		const response = data.result;
+
+		const jsonRegex = /•••([^•]+)•••/;
+		const matches = response.match(jsonRegex);
+
+		if (matches && matches.length > 1) {
+			const jsonString = matches[1];
+			const jsonObject = JSON.parse(jsonString);
+			const cleanedHtml = response.replace(matches[0], "");
+			return [cleanedHtml, jsonObject];
+		}
+	}
+
+	const introRef = useRef<HTMLDivElement>(null);
+	const inputBoxRef = useRef<HTMLDivElement>(null);
+	function updateHTML(html: string) {
+		const children = introRef.current?.children;
+		if (children) {
+			gsap.to(children, {
+				duration: 0.4,
+				opacity: 0,
+				y: -20,
+				onComplete: () => {
+					while (children.length > 0) {
+						children[0].remove();
+					}
+					introRef.current?.insertAdjacentHTML("beforeend", html);
+					const newChildren = introRef.current?.children;
+					if (newChildren) {
+						gsap.fromTo(
+							newChildren,
+							{ opacity: 0, y: 20 },
+							{ duration: 0.4, opacity: 1, y: 0 }
+						);
+					}
+					gsap.to(inputBoxRef.current, {
+						duration: 0.4,
+						opacity: 0,
+						scale: 0,
+						onComplete: () => {
+							inputBoxRef.current?.remove();
+						}
+					});
+					gsap.to(btnRef.current, {
+						duration: 0.4,
+						opacity: 0,
+						scale: 0,
+						onComplete: () => {
+							btnRef.current?.remove();
+						}
+					});
+					
+				},
+			});
+		}
+	}
+
 	return (
 		<>
 			<Head>
@@ -59,32 +168,48 @@ const App = () => {
 
 			<main id="aboutPage" className={styles.main}>
 				<div className={styles.inner}>
-					<div className={styles.intro}>
-						<h2>What you want to learn about?</h2>
-						<p>
-							This is a platform enhanced using AI to help students learn about
-							topics in a short & fun way. An additional quiz of 5 questions is
-							also generated for you to practice with and keep growing.
-						</p>
-					</div>
-					<div
-						className={styles.inputBox}
-						onClick={() => {
-							inputRef.current?.focus();
-						}}
-					>
-						<div className={styles.before}>More about</div>
-						<input
-							ref={inputRef}
-							type="text"
-							placeholder={prompt}
-							maxLength={100}
-							className={styles.textArea}
-						/>
-					</div>
-					<div className={styles.proceed}>
-						{" "}
-						Proceed <span className={styles.arrow}>→</span>
+					<div className={styles.container}>
+						<div>
+							<div className={styles.intro} ref={introRef}>
+								<h2>What you want to learn about?</h2>
+								<p>
+									This is an AI powered platform made to teach students about
+									topics ranging from simple mathematics to the most advanced
+									quantum theory, you can generate 5 additional questions for
+									each prompt to practice and learn about the concepts.
+								</p>
+							</div>
+							<div
+								className={styles.inputBox}
+								ref={inputBoxRef}
+								onClick={() => {
+									inputRef.current?.focus();
+								}}
+							>
+								<div className={styles.before}>More about</div>
+								<input
+									ref={inputRef}
+									type="text"
+									placeholder={prompt}
+									maxLength={100}
+									className={styles.textArea}
+								/>
+							</div>
+							<div className={styles.proceed} ref={btnRef}>
+								Proceed <span className={styles.arrow}>→</span>
+								<div
+									className={styles.loading}
+									ref={loadingRef}
+									style={{ visibility: "hidden" }}
+								>
+									<span className={styles.loader}></span>loading
+								</div>
+							</div>
+
+							<div className={styles.lottiePanel}>
+								<div className={styles.lottie}></div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</main>

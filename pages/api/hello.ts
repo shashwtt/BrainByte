d519e43cfc6cc6defd4e5ad14a1e5ab9a67a7575
cy-bug <shashwat5590@gmail.com>
2023-05-14 01:31:21 +0530

    OpenAI API

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,27 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Configuration, OpenAIApi } from 'openai';
 
-type Data = {
-  name: string
-}
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const completion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `explain the given prompt like i'm a toddler in brief, not more than 200 words, give simple analogy for the same if applicable (important: wrap headings in <h2> tag and text in <p> tag). Next I want you to give me a json code for a 5 question quiz (with 4 options and correct answer) wrapped inside  like this "••• {json code} ••• "
+
+    Topic —> ${req.body.text}
+    
+    `,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 1500,
+  });
+  res.status(200).json({ result: completion.data });
 }
